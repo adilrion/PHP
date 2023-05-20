c2ad8793b65c2c87054a1cb0a76756a38f635aa1
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2023-05-21 01:32:55 +0600

    learn array, associative array, multidimensional array, associative multidimensional array

--- index.php
+++ index.php
@@ -15,9 +15,51 @@
     $name = 'Adil Mahmoud Rion';
     echo 'My Name Is ' . $name . '</br>';
     $age = 12;
-    print("I'm .$age. Years old");
+    print("I'm .$age. Years old </br>");
 
 
+    $food = array('Appall', 'Banana', 'Mango');
+    echo "I like  .$food[2]. </br>";
+    echo count($food);
+
+
+    // Loop Through an Indexed Array
+    for ($x = 0; $x < count($food); $x++) {
+        echo "<br>Array index $x : $food[$x]";
+    }
+
+
+    // Associative Array 
+    $food2 = array('Appall' => '10kg', 'Banana' => '5kg', 'Mango' => '30kg');
+
+    // Loop Through an Associative Indexed Array
+    foreach ($food2 as $key => $value) {
+        echo "<br> $key : $value <br>";
+    }
+    // access values in an associative multidimensional array
+    $assocMultiDimArray = array(
+        'fruits' => array(
+            'apple' => 'red',
+            'banana' => 'yellow',
+            'mango' => 'orange'
+        ),
+        'animals' => array(
+            'lion' => 'roar',
+            'elephant' => 'trumpet',
+            'giraffe' => 'hum'
+        )
+    );
+
+    // Accessing all values in an associative multidimensional array
+    foreach ($assocMultiDimArray as $category => $subArray) {
+        echo "<br>Category: " . $category . "<br>";
+
+        foreach ($subArray as $key => $value) {
+            echo "Key: " . $key . ", Value: " . $value . "<br>";
+        }
+
+        echo "<br>";
+    }
 
 
 
